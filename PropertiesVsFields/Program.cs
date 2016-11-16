357a19569a9079f972e2ff4a25140ef52f20886f
Mohamed Elkammar <melkammar@invensense.com>
2016-11-15 16:27:29 -0800

    Initial commit. Used to generate data in debug and release modes.

--- PropertiesVsFields/Program.cs
+++ PropertiesVsFields/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace PropertiesVsFields
 {
@@ -10,6 +7,67 @@ namespace PropertiesVsFields
     {
         static void Main(string[] args)
         {
+            ReadySetGo go = new ReadySetGo();
+
+            go.Run();
+
+            Console.ReadKey();
         }
+    }
+
+
+    class ReadySetGo
+    {
+
+        public void Run()
+        {
+            const int iterations = (int)1e7;
+
+            var r = new Random();
+            FieldClass[] f = new FieldClass[iterations];
+            PropertyClass[] p = new PropertyClass[iterations];
+
+            for (int i = 0; i < iterations; i++)
+            {
+                f[i] = new FieldClass() { Number = r.Next() };
+                p[i] = new PropertyClass() { Number = r.Next() };
+            }
+
+            int sum1 = 0, sum2 = 0;
+
+            var s = new Stopwatch();
+            s.Start();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                sum1 += f[i].Number;
+            }
+
+            s.Stop();
+
+            Console.WriteLine("Field operations took {0} ticks.", s.ElapsedTicks);
+
+            s.Reset();
+            s.Start();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                sum2 += p[i].Number;
+            }
+
+            s.Stop();
+
+            Console.WriteLine("Property operations took {0} ticks.", s.ElapsedTicks);
+        }
+    }
+
+    class FieldClass
+    {
+        public int Number;
+    }
+
+    class PropertyClass
+    {
+        public int Number { get; set; }
     }
 }
